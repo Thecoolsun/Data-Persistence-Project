5821b4b79285cec26fd2cf7ace59e5a35934cf9c
Jérémie LERIGAB <84409310+Thecoolsun@users.noreply.github.com>
2025-05-09 19:11:34 +0200

    removed useless comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,10 +26,10 @@ public class GameManager : MonoBehaviour
     }
 
 
-    /// Handles the game state transitions based on user input.
-    /// If the left mouse button is released, sets the game over flag to false and increments the score.
-    /// If the right mouse button is released, sets the game over flag to true, updates the high score
-    /// If the current score is higher, and resets the current score.
+
+    /// Checks every frame if the game is over by checking the game over flag in MainManager.
+    /// If the game is over and the game over flag in this script is false, sets the high score
+    /// and sets the game over flag to true.
     void Update()
     {
         mainGameOver = GameObject.Find("MainManager").GetComponent<MainManager>().m_GameOver;
@@ -38,50 +38,15 @@ public class GameManager : MonoBehaviour
         {
             SetHighScore();
             gameOver = true;
-            // ResetScore();
         }
-
-        // if (Input.GetKeyUp(KeyCode.Mouse0))
-        // {
-        //     gameOver = false;
-        //     UpdateScore(1);
-        // }
-        // else if (Input.GetKeyUp(KeyCode.Mouse1))
-        // {
-        //     SetHighScore();
-        //     gameOver = true;
-        //     ResetScore();
-        // }
-    }
-
-    // ------------  SCORE EDITING  ------------
-
-    /// Updates the player's score by adding the specified amount.
-    /// If the game is not over, increments the score, updates the score display,
-    /// and sets the player's current score in the DataManager.
-    // private void UpdateScore(int scoreToAdd)
-    // {
-    //     if (gameOver == false)
-    //     {
-    //         score += scoreToAdd;
-    //         scoreText.text = $"Score : {score}";
-    //         DataManager.Instance.playerScore = score;
-    //     }
-    // }
-
-    /// Resets the current score to 0 and updates the score display.
-    private void ResetScore()
-    {
-        score = 0;
-        scoreText.text = $"Score : {score}";
     }
 
     // ------------  SCORE SETTING  --------------
 
-    /// Updates the high score and high score name if the player's current score exceeds the current high score.
-    /// If the player's name is not the same as the current high score name, updates the high score name.
-    /// If the player's current score is higher than the current high score, updates the high score and
-    /// displays the updated high score and name.
+    /// Updates the high score and high score name if the current score is higher.
+    /// The high score name is only updated if the current player's name is different
+    /// from the current high score name.
+    /// Should be called when the game is over.
     private void SetHighScore()
     {
         if (score > DataManager.Instance.playerHighScore)
@@ -93,11 +58,6 @@ public class GameManager : MonoBehaviour
             {
                 DataManager.Instance.playerHighScoreName = DataManager.Instance.playerName;
             }
-
-            // if (DataManager.Instance.playerScore > DataManager.Instance.playerHighScore)
-            // {
-            //     DataManager.Instance.playerHighScore = highScore;
-            // }
 
             highScoreText.text = $"Best score : {DataManager.Instance.playerHighScoreName} : {DataManager.Instance.playerHighScore}";
         }
